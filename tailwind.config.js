312a0a63582d12093180df56bdf378dfa90ea553
garettg <garettg@gmail.com>
2020-08-18 15:06:11 -0700

    Config change to reduce package size.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+	purge: {
+    	enabled: true,
+    	content: [
+			'./src/*.html',
+			'./src/*.tsx',
+			'./src/*.jsx',
+			'./src/*.vue',
+		],
+  	},
 	future: {
     	removeDeprecatedGapUtilities: true,
   	},
