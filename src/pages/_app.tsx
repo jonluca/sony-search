61e3e6689e3ee1985cc389279f81972d9f57ee25
JonLuca DeCaro <jonluca.decaro@gmail.com>
2022-08-19 14:55:14 -0400

    better css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from "next/app";
 import { GoogleAnalytics, usePageViews, event } from "nextjs-google-analytics";
 import type { NextWebVitalsMetric } from "next/app";
-import "../index.css";
+import "..//index.css";
+import Head from "next/head";
 
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
@@ -21,6 +22,20 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <title>Sony Alpha Search</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/svg+xml" href="/static/icons/favicon.svg" />
+        <link rel="alternate icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+        <link rel="alternate icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+        <link rel="alternate shortcut icon" href="/static/icons/favicon.ico" />
+        <link rel="manifest" href="/static/icons/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
+      </Head>
       <div className="mainContent">
         <GoogleAnalytics gaMeasurementId={"G-BTJMKGNN1B"} />
         <Component {...pageProps} />
